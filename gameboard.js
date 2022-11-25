f0321c63e0d658591c7634622be435087575ca48
tidynoob <mitchellgriffin9@gmail.com>
2022-11-25 16:34:55 -0600

    feat: gameboard records hits and if all ships are sunk

--- gameboard.js
+++ gameboard.js
@@ -5,34 +5,81 @@ const gameboard = () => {
 
   let _count = 0;
 
-  const makeID = () => _count++;
+  const _hitSpots = [];
+  const _missedSpots = [];
+
+  const arraysEqual = (a, b) => {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (let i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  };
+
+  const getHitSpots = () => _hitSpots;
+
+  const getMissedSpots = () => _missedSpots;
+
+  const _makeID = () => _count++;
 
   const placeShip = (x, y, length, direction) => {
     // check if there's already a ship there
     for (let i = 0; i < length; i++) {
-      if (direction === 'vertical') {
+      if (direction === 'v') {
         if (board[y + i][x] !== null) {
           return false;
         }
-      } else if (direction === 'horizontal') {
+      } else if (direction === 'h') {
         if (board[y][x + i] !== null) {
           return false;
         }
       }
     }
     // place the ship
-    const ID = makeID();
+    const ID = _makeID();
     for (let i = 0; i < length; i++) {
-      if (direction === 'vertical') {
+      if (direction === 'v') {
         board[y + i][x] = ship(length, ID);
-      } else if (direction === 'horizontal') {
+      } else if (direction === 'h') {
         board[y][x + i] = ship(length, ID);
+      } else {
+        return false;
       }
     }
     return true;
   };
 
-  return { board, placeShip };
+  const hit = (x, y) => {
+    if (_missedSpots.some((spot) => arraysEqual(spot, [x, y]))) {
+      throw new Error('Location already missed');
+    }
+    if (board[y][x] === null) {
+      _missedSpots.push([x, y]);
+      throw new Error('No ship at that location');
+    }
+
+    if (_hitSpots.some((spot) => arraysEqual(spot, [x, y]))) {
+      throw new Error('Location already hit');
+    }
+    const ID = board[y][x].getID();
+    board.map((row) => row.map((cell) => {
+      if (cell !== null && cell.getID() === ID) {
+        cell.hit();
+      }
+      return cell;
+    }));
+    _hitSpots.push([x, y]);
+    return true;
+  };
+
+  const allSunk = () => board.every((row) => row.every((cell) => cell === null || cell.isSunk()));
+
+  return {
+    board, placeShip, hit, getHitSpots, getMissedSpots, allSunk,
+  };
 };
 
 export default gameboard;
--- gameboard.test.js
+++ gameboard.test.js
@@ -22,33 +22,81 @@ test("gameboard is setup as empty arrays", () => {
   ]);
 });
 
-test("gameboard can place a ship", () => {
-  testBoard.placeShip(0, 0, 3, "vertical");
-  expect(testBoard.board).toEqual([
-    [expect.any(Object), null, null, null, null, null, null],
-    [expect.any(Object), null, null, null, null, null, null],
-    [expect.any(Object), null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-  ]);
+test("gameboard can place a ship vertically", () => {
+  testBoard.placeShip(0, 0, 3, "v");
+  expect(testBoard.board[0][0]).not.toBeNull();
+  expect(testBoard.board[1][0]).not.toBeNull();
+  expect(testBoard.board[2][0]).not.toBeNull();
+});
+
+test("first ship has ID 0", () => {
+  expect(testBoard.board[0][0].getID()).toBe(0);
 });
 
 test("gameboard can place a ship horizontally", () => {
-    testBoard.placeShip(0, 1, 3, "horizontal");
-    expect(testBoard.board).toEqual([
-        [expect.any(Object), expect.any(Object), expect.any(Object), expect.any(Object), null, null, null],
-        [expect.any(Object), null, null, null, null, null, null],
-        [expect.any(Object), null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-    ]);
+    testBoard.placeShip(1, 0, 3, "h");
+    expect(testBoard.board[0][1]).not.toBeNull();
+    expect(testBoard.board[0][2]).not.toBeNull();
+    expect(testBoard.board[0][3]).not.toBeNull();
 });
 
-// test("ships can be hit", () => {
-//   testBoard.hit(0, 0);
-//   expect(testBoard.board[0][0].ship.hp()).toBe(2);
-// });
+test("second ship has ID 1", () => {
+    expect(testBoard.board[0][1]).toEqual({
+        getID: expect.any(Function),
+        hp: expect.any(Function),
+        hit: expect.any(Function),
+        isSunk: expect.any(Function),
+        });
+    expect(testBoard.board[0][1].getID()).toBe(1);
+    });
+
+test("different ships have different IDs", () => {
+    expect(testBoard.board[0][0].getID() == testBoard.board[0][1].getID()).toBe(false);
+});
+
+test("gameboard objects are ships", () => {
+  expect(testBoard.board[0][0].hp()).toBe(3);
+  expect(testBoard.board[0][0].isSunk()).toBe(false);
+});
+
+test("ships can be hit", () => {
+  testBoard.hit(0, 0);
+  expect(testBoard.board[0][0].hp()).toBe(2);
+});
+
+test("hitting a location adds it to hitSpots", () => {
+  expect(testBoard.getHitSpots()).toContainEqual([0, 0]);
+});
+
+test("all ships of same ID are hit", () => {
+  testBoard.hit(0, 1);
+  expect(testBoard.board[0][0].hp()).toBe(1);
+  expect(testBoard.board[1][0].hp()).toBe(1);
+  expect(testBoard.board[2][0].hp()).toBe(1);
+});
+
+test("location can only be hit once", () => {
+  expect(testBoard.getHitSpots()).toContainEqual([0, 0]);
+  expect(() => testBoard.hit(0, 0)).toThrow('Location already hit');
+  });
+
+test("ships can be sunk", () => {
+  testBoard.hit(0, 2);
+  expect(testBoard.board[0][0].isSunk()).toBe(true);
+  expect(testBoard.board[1][0].isSunk()).toBe(true);
+  expect(testBoard.board[2][0].isSunk()).toBe(true);
+
+});
+
+test("sunk ships cannot be hit", () => {
+  expect(() => testBoard.hit(0, 0)).toThrow('Location already hit');
+  expect(() => testBoard.hit(0, 1)).toThrow('Location already hit');
+  expect(() => testBoard.hit(0, 2)).toThrow('Location already hit');
+});
+
+test("gameboard properly checks if all ships are sunk", () => {
+  testBoard.hit(1,0);
+  testBoard.hit(2,0);
+  testBoard.hit(3,0);
+  expect(testBoard.allSunk()).toBe(true);
+});
